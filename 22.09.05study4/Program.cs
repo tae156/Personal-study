65d17d1a46bd5f7162da401b52d577f200bcb92f
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 01:41:01 +0900

    1.4 실습 프로젝트 생성

---
+++ 22.09.05study4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._05study4
+{
+    public struct Mystruct
+    {
+        public int Age;
+    }
+    class MyClass
+    {
+        public int Age;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mystruct Test1 = new Mystruct();
+            Test1.Age = 12;
+            Mystruct Test2 = Test1;
+            Test2.Age = 24;
+            Console.WriteLine("{0} {1}", Test1.Age, Test2.Age);
+
+            MyClass Test3 = new MyClass();
+            Test3.Age = 12;
+            MyClass Test4 = Test3;
+            Test4.Age = 24;
+            Console.WriteLine("{0} {1}", Test3.Age, Test4.Age);
+        }
+    }
+}
