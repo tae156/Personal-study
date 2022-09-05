2ba0b7e344f99999e0b9d7f1c735b764f417f486
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 01:32:57 +0900

    1.3 실습 프로젝트 생성
    구조체를 같은 구조체에 대입하게 되면 값이 복사 됨

---
+++ 22.09.05study3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._05study3
+{
+    public struct Mystruct
+    {
+        public int Age;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mystruct Teststruct1, Teststruct2;
+            Teststruct2.Age = 10;
+            Teststruct1 = Teststruct2;
+            Console.WriteLine("{0}", Teststruct1.Age);
+
+            Mystruct test;
+            test = new Mystruct();
+            Console.WriteLine(test.Age);
+        }
+    }
+}
