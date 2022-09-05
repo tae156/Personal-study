09c7a84dc197a19d656f31a3c1256bdf9998301b
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 01:44:45 +0900

    1.5 실습 프로젝트 생성

---
+++ 22.09.05study5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._05study5
+{
+    public struct Mystruct
+    {
+        public int Age;
+        public float Num2;
+        public bool IsReady;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mystruct Test1 = new Mystruct();
+            Console.WriteLine("{0} {1} {2}", Test1.IsReady, Test1.Age, Test1.Num2);
+        }
+    }
+}
