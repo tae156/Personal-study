50905e711f87cb646c819a15d904da1e212124d1
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 00:53:11 +0900

    1.2 실습 프로젝트 생성
    구조체에 생성자와 소멸자를 명시하고 new로 생성했을 때와 일반 선언했을 때의 초기화 한다.

---
+++ 22.09.05study2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._05study2
+{
+    public struct Mystruct
+    {
+        public int Age;
+        public Mystruct(int InAge)
+        {
+            Age = InAge;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mystruct TestStruct1;
+            TestStruct1.Age = 12;
+            Console.WriteLine(TestStruct1.Age);
+
+            Mystruct Teststruct2 = new Mystruct();
+            Console.WriteLine(Teststruct2.Age);
+
+            Mystruct Teststruct3 = new Mystruct();
+            Console.WriteLine(Teststruct3.Age);
+        }
+    }
+}
