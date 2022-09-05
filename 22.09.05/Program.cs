e87e81e6ea97912d9546f5cd84984438f6fa5bc9
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 00:34:58 +0900

    초기 커밋.

---
+++ 22.09.05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._05
+{
+    internal class Program
+    {
+
+        public struct Mystruct
+        {
+            public const float PI = 13.3f; //자동으로 정적변수와 동일하게 적용이 됨
+            public static float Age = 10.22f; //static인 정적 변수를 사용하고있음
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} , {1}", Mystruct.PI, Mystruct.Age);
+        }
+    }
+}
