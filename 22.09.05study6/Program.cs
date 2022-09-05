69d4bc4adedeff038454b92d416f3bebd4996c0c
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 01:53:29 +0900

    1.6 실습 프로젝트 생성
    국어, 영어, 수학 점수를 구조체 멤버 변수로 입력하고 Compute() 메서드를 호출하면 평균과 총점이 계산되도록 하여 프로그래밍 작성

---
+++ 22.09.05study6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._05study6
+{
+    public struct Mystruct
+    {
+        public int Kor, Eng, Math, Total1;
+        public float Average;
+
+        public void Computer()
+        {
+            Total1 = Kor + Eng + Math;
+            Average = Total1 / 3.0f;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mystruct Test1 = new Mystruct();
+            Test1.Kor = 80;
+            Test1.Eng = 99;
+            Test1.Math = 100;
+            Test1.Computer();
+            Console.WriteLine("총점 : {0}  평균: {1}",Test1.Total1, Test1.Average);
+        }
+    }
+}
