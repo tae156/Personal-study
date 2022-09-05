70301988067f440f9210ac25ca0aa5bd158b4c11
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 01:59:43 +0900

    1.7 실습 프로젝트 생성
    열거형 변수가 아닌 변수에 열거형 값을 대입할 때는 데이터형을 명시할 것

---
+++ 22.09.05study7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._05study7
+{
+    enum Days { Sun = 1, Mon, Tue, Wed, Thu, Fri, Sat};
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nvalue = (int)Days.Mon;
+            Days day = Days.Tue;
+            Console.WriteLine("{0} {1}", nvalue, day);
+        }
+    }
+}
