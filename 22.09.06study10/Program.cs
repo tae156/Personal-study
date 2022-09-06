e89a60922a0146913560eb1c72e5a3ae19041351
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 23:55:58 +0900

    2.10 실습 프로젝트 생성
    try ~ catch문은 한 프로젝트를 개발할 때 사용자가 오류 부분을 찾기 위해 사용되는 내용

---
+++ 22.09.06study10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06studyA10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = { 1, 2, 3 };// 배열 데이터 순서는 0부터 시작
+            try
+            {
+                Array[3] = 10;
+            }
+            catch(IndexOutOfRangeException e) 
+            {
+                Console.WriteLine("예러 발생");
+                Console.WriteLine(e.ToString());
+                Array[2] = 10;
+            }
+            for(int i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine("{0}", Array[i]);
+            }
+            //데이터 집합에 마지막 초기값을 설정하는 시나리오 방식이였던거
+        }
+    }
+}
