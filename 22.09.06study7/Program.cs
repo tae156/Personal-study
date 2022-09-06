912fd4a849abdb5f459116000c85b4cc64ae1c5b
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 23:19:22 +0900

    2.7 실습 프로젝트 생성
    한개의 데이터타입에 어려게 사용되는 값을 switch문 이용하여 원하는 값을 찾을 때 사용되는 내용

---
+++ 22.09.06study7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06study7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  int num = 1;
+              switch (num)
+              {
+                  case 1:
+                      Console.WriteLine("1입니다.");
+                      break;
+                  case 2:
+                      Console.WriteLine("2입니다.");
+                      break ;
+
+              }*/
+
+            /*   string str = "yes";
+               switch(str)
+               {
+                   case "yes":
+                       Console.WriteLine("yes 입니다.");
+                       break;
+                   case "no":
+                       Console.WriteLine("no 입니다.");
+                       break;
+               }*/
+
+            char a = 'A';
+            switch (a)
+            {
+                case 'A':
+                    Console.WriteLine("A 입니다.");
+                    break;
+                case 'B':
+                    Console.WriteLine("B 입니다.");
+                    break;
+            }
+
+        }
+    }
+}
