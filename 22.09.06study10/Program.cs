02cbdb6014bf4f0c11fc8d115f33bb58f96b7fa8
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-07 00:06:08 +0900

    2.10 추가 실습
    아무리 지역변수를 사용해도 try ~ catch문에 초기값을 사용하면 안되는 내용

--- 22.09.06study10/Program.cs
+++ 22.09.06study10/Program.cs
@@ -10,7 +10,7 @@ namespace _22._09._06studyA10
     {
         static void Main(string[] args)
         {
-            int[] Array = { 1, 2, 3 };// 배열 데이터 순서는 0부터 시작
+            /*int[] Array = { 1, 2, 3 };// 배열 데이터 순서는 0부터 시작
             try
             {
                 Array[3] = 10;
@@ -25,7 +25,22 @@ namespace _22._09._06studyA10
             {
                 Console.WriteLine("{0}", Array[i]);
             }
-            //데이터 집합에 마지막 초기값을 설정하는 시나리오 방식이였던거
+            //데이터 집합에 마지막 초기값을 설정하는 시나리오 방식이였던거*/
+
+            int m; //이 데이터형은 지역변수를 사용하고 있음
+            try
+            {
+                m = 12;
+                Console.WriteLine("{0}",m);
+            }
+            catch
+            {
+                Console.WriteLine("예외 발생");
+            }
+            Console.WriteLine("try문 밖에서 변수 출력 {0}", m);
+            //현제 초기 값을 가지고 있지 않아 에러 표출이 되고 있는데
+            //try안에서 초기값이 이루어지고 있고 그 외 밖에서 사용이 불가능하다.
+
         }
     }
 }
