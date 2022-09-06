2a5a6803ca39ada8143d4298a90f2bf19fa7efdd
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 22:57:07 +0900

    2.4 실습 프로젝트 생성
    as 연산자를 사용하여 class별 박싱/언박싱으로 통해 null값이 비교되고 있는지 확인하는 내용

---
+++ 22.09.06study4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06study4
+{
+    class A
+    {
+
+    }
+    class B
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "123";
+            object obj1 = str1;
+            string str2 = obj1 as string;
+            Console.WriteLine(str2);
+
+            A Test1 = new A();
+            object obj2 = Test1;
+            B Test2 = obj2 as B;
+            if (Test2 == null)
+                Console.WriteLine("호환 실패");
+            else
+                Console.WriteLine("호환 성공");
+        }
+    }
+}
