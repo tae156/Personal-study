4a42e80545e7e605c7c3c4c15a5ad53b6c942702
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 23:05:10 +0900

    2.6 실습 프로젝트 생성
    if ~ else문 사용하는 내용

---
+++ 22.09.06study6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06study6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            if (true)
+                Console.WriteLine("hello World");//조건문이 true이기 때문에 무조건 실행
+            else
+                Console.WriteLine("C# Progamming");
+        }
+    }
+}
