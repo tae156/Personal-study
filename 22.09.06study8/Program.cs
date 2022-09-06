c8d3f10022fdd114cb23a06b100383fee5007df6
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 23:21:59 +0900

    2.8 실습 프로젝트 생성
    for문을 이용하여 0 ~ 4까지 생성되는 내용

---
+++ 22.09.06study8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06study8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            for(int i = 0; i < num; i++)
+            {
+                Console.WriteLine("{0}",i);
+
+            }
+        }
+    }
+}
