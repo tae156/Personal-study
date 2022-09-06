741108246d92b6e17fc78335dc4497478983806a
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 23:32:51 +0900

    2.9 실습 프로젝트 내용
    두 개의 실습 내용을 보았을 때 배열 안에 사용되는 숫자가 렌덤일 때 순차적으로 표현되지만
    ArrayLinst인 컬렉션일 경우 숫자가 순서대로 나열되는게 아닌 List 값이 m에 데이터를 넣어주는대로 표현되는 것이다.

---
+++ 22.09.06study9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06study9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           /* int[] Array = { 1, 2, 3, 4, 5, 6, };
+            foreach( int value in Array) // 순서대로 나열되어있는 값을 표출
+            {
+                Console.WriteLine(value);
+            }*/
+           
+            //데이터형 또는 클래스 단위를 순차적으로 나열할 때 사용되는 내용
+            ArrayList List = new ArrayList();
+            List.Add(1);
+            List.Add(2);
+            List.Add(3);
+            foreach(int m in List)
+            {
+                Console.WriteLine(m);
+            }
+
+        }
+    }
+}
