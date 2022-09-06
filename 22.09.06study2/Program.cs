1f98fa891cc54b5caa27e006c658cbc6c40b5da3
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 21:53:19 +0900

    2.2 실습 프로젝트 생성
    산술 연산자 string(문자열)함수를 사용하여 정수/부동(소수) + 문자열을 연결되어있으면 어떤 결과 값을 나올지 확인하는 내용

---
+++ 22.09.06study2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06study2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "3" + ".14";
+            Console.WriteLine(+5); //양 5 (거의 사용되지 않음
+            Console.WriteLine(5+5);// 양 10
+            Console.WriteLine(5+.5);// 소수 5.5
+            Console.WriteLine("5" + "5"); // 문자열 연결식 55
+            Console.WriteLine(5.01f + "5");// 문자열 연결식 5.015
+            Console.WriteLine(3.14f + "5");//문자열 연길식 3.145
+            Console.WriteLine(str);
+
+            str = 1 + "test" + 3.14 + "abcd";
+            Console.WriteLine(str);
+        }
+    }
+}
