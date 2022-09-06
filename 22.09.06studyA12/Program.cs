119f9a057a3ff14c9b53252311471bb8402b0664
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-07 00:28:30 +0900

    2.12 실습 프로젝트 생성
    try ~ catch문을 사용하지 않고 throw을 이용하여 임의적으로 예외처리 처리하는 내용

---
+++ 22.09.06studyA12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06studyA12
+{
+    internal class Program
+    {
+        static int GetNumber(int index)
+        {
+            int[] num = { 300, 600, 900 };
+            if(index >= num.Length)
+            {
+                throw new IndexOutOfRangeException();//인의적으로 예외처리 만듬
+            }
+            return num[index];
+        }
+        static void Main(string[] args)
+        {
+            int value = GetNumber(3);
+        }
+    }
+}
