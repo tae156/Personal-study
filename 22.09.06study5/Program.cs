16450b2fea29471816d7c89cc02f2ba67ab56155
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 22:57:46 +0900

    2.5 실습 프로젝트 생성
    null병합 연산자를 조사하기 위한 내용

---
+++ 22.09.06study5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06study5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+            int y = x ?? -1;
+            Console.WriteLine(y);
+
+            x = 10;
+            y = x ?? -1;
+            Console.WriteLine(y);
+        }
+    }
+}
