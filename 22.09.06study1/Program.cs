020f75c3d70434efc2e71a4a2fa5bfd4c328fd52
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-06 21:51:09 +0900

    2.1 실습 프로젝트 생성
    다한 연산식 "!"은 bool형에 사용되는 내용

---
+++ 22.09.06study1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._06study1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool BFlag = false;
+            Console.WriteLine("{0} {1} {2}", !BFlag, !true, !false);
+            //!BFlag false로 초기화 되어있으니 앞 다항연자사를 사용했을 때 true로 변경된다.
+
+        }
+    }
+}
