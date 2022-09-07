2ea705d8732db0aa8b52e94ea426283b4c289b86
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-07 23:51:03 +0900

    3.1 실습 프로젝트 생성
    배열을 사용하여 for문 및 foreach를 이용한 내용

---
+++ 22.09.07study1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._07study1
+{
+    internal class Program
+    {
+        static void Main(string[] args)//배열을 이용한 반복문으로 출력하는 내용
+        {
+            int[] array = { 1, 2, 3, 4 };
+            for(int i=0; i<array.Length; i++)
+            {
+                Console.WriteLine("{0}", array[i]);
+            }
+
+            foreach(int m in array) 
+                Console.WriteLine("{0}", m);
+
+            string[] mr = { "월", "화", "수", "목", "금", "토", "월" };
+            foreach (string str in mr)
+                Console.WriteLine(str + " ");
+        }
+    }
+}
