6037021f61e41fed2d86e1c3eb648f069d6a5700
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-07 23:51:35 +0900

    2.2 실습 프로젝트 생성
    이차원 배열 및 다차원 배열 이용하는 방법

---
+++ 22.09.07study2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._07study2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[2, 2];
+            array[0, 0] = 1;
+            array[0, 1] = 2;
+            array[1, 0] = 3;
+            array[1, 1] = 4;
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                    
+                }
+                
+            }
+            Console.Write("\n");
+
+            int[,,] Earray = { {{ 1,2}, { 3,4} },
+                                 { {5,6 },{7, 8 } } };
+            for(int i= 0; i< 2; i++)
+            {
+                for(int j=0; j<2; j++)
+                {
+                    for(int k = 0; k <2; k++)
+                    {
+                        Console.Write(Earray[i, j, k] + " "); 
+                    }
+                    
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
