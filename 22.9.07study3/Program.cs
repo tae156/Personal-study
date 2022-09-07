a467d14515f7f9e05dd0006d9c5a190cb55eed1b
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-08 00:08:28 +0900

    2.3 실습 프로젝트 생성
    가변 배열을 활용한 내용

---
+++ 22.9.07study3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._9._07study3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = new int[2][]; //행 메모리값: 2
+            array[0] = new int[]{ 1,2,3}; //여기서 열의 메모리 값은 3이란 것을 알 수 있음
+            array[1] = new int[]{ 4,5,6 };
+
+            for(int i = 0; i< array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+            }
+        }
+    }
+}
