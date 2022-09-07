eab207213408a91c3a66e2bd6124798f247db095
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-08 00:33:47 +0900

    2.5 실습 프로젝트 생성
    일차원 배열을 만들고 참소 배열 변수를 생성하여 Main메소드에 참조를 가지고와서 사용하는 내용

---
+++ 22.09.07study5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._07study5
+{
+    internal class Program
+    {
+        static void Array(string[] arr) //참소 변수를 생성하여 영문을 한문으로 변경하는 내용
+        {
+            string[] Han = { "월", "화", "수", "목", "금", "토", "일" };
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Han[i];
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] date = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+            Array(date);
+            foreach(string str in date)
+            {
+                Console.WriteLine(str + " ");
+            }
+        }
+    }
+}
