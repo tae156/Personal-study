4b08d94be7946e58609012743cdff77b452cab47
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-13 21:45:55 +0900

    4.1 실습 프로젝트 생성
    참조형 클래스의 1차원 배열 과 2차원 배열을 생성하여
    결과 값 출력하는 내용

---
+++ 22.09.13study1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._13study1
+{
+    internal class Program
+    {
+        static int[] CreateArray1(int nSize)
+        {
+            int[] Array1 = new int[nSize];
+            for(int i= 0; i<Array1.Length; i++)
+            {
+                Array1[i] = i;
+            }
+            return Array1;
+        }
+
+        static int[,] CreateArray2(int nRow, int Col)
+        {
+            int index = 0;
+            int[,] Array2 = new int [nRow, Col];
+            for(int i= 0; i < nRow; i++)
+            {
+                for(int j = 0; j < Col; j++)
+                {
+                    Array2[i, j] = index++;
+                }
+            }
+            return Array2;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nArray1;// 참조배열변수
+            int[,] nArray2; 
+
+            nArray1 = CreateArray1(5);
+            nArray2 = CreateArray2(2, 3);
+
+            for(int i= 0; i < nArray1.Length; i++)
+            {
+                Console.Write(nArray1[i]);
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(nArray2[i, j]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
