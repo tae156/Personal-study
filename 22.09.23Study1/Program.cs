5858a63696b3fc35e3229f50fa19b651d76f484a
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-24 02:17:11 +0900

    5.1 실습 프로젝트 생성
    파일생성하여 쓰기 또는 읽기 사용법

---
+++ 22.09.23Study1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._23Study1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 12;
+            float value2 = 3.14f;
+            string str1 = "Hello World";
+            FileStream fs = new FileStream("test.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(value);
+            sw.Write(value2);
+            sw.Write(str1);
+            sw.Close();
+
+        }
+    }
+}
