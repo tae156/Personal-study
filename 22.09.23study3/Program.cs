b376c38240595197c6b79bc67cc2e25a6ba33ba0
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-24 02:21:26 +0900

    5.3 실습 프로젝트 생성
    이전에 사용되었던  text.txt파일을 현재 프로젝트파일 안에 가지고 와서 읽기 요요도로만 사용되는 방법

---
+++ 22.09.23study3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._23study3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("text.txt", FileMode.Open, FileAccess.Read);//파일 읽기로만 사용하겠다.
+            StreamReader sr = new StreamReader(fs);
+            int value = int.Parse(sr.ReadLine()); // 문자열로 읽어들임
+            float value2 = float.Parse(sr.ReadLine());
+            string str1 = sr.ReadLine();
+            sr.Close();
+            Console.WriteLine("{0}, {1}, {2}", value, value2, str1);
+
+        }
+    }
+}
