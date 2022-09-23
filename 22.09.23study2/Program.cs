f5b726f39515c7d41d6141f469fcb850f2bba555
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-24 02:20:33 +0900

    5.2 실습 프로젝트 생성
    FileStream을 이용하여 파일 생성하고 읽기 또는 쓰기 나누어 사용했으나,  StreamWriter은 쓰기 용도로만 사용하고 끝내는 방식(파일 생성도 할 수 있음)

---
+++ 22.09.23study2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._23study2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 10;
+            float value2 = 3.14f;
+            string str = "Helio";
+
+            StreamWriter sw = new StreamWriter("text.txt");
+            sw.WriteLine(value);
+            sw.WriteLine(value2);
+            sw.WriteLine(str);
+            sw.Close();
+        }
+    }
+}
