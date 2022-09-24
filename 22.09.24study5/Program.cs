81dc879313e9531df82612ff4a72b761f8692438
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-25 02:42:28 +0900

    6.5 실습 프로젝트 생성
    이전에 저장된 파일을 읽는 방식인며 활장자 명이 .dat로 되어있어도 프로그램에서 출력되는 값은 정상적으로 받아온다.

---
+++ 22.09.24study5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._24study5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1;
+            float var2;
+            string str1;
+
+            using (BinaryReader br = new BinaryReader(File.Open("test.dat", FileMode.Open)))
+            {
+                var1 = br.ReadInt32();
+                var2 = br.ReadSingle();
+                str1 = br.ReadString();
+            }
+            Console.WriteLine("{0} {1} {2}", var1, var2, str1);
+        }
+    }
+}
