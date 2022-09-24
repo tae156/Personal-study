d54fdea384d8b9d6c325da7db81fc484ec125bfa
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-25 02:41:21 +0900

    6.4 실습 프로젝트 생성
    이진파일 읽기 와 쓰기 내용
    확장자명이 .dat임으로 파일을 확인할 수 없음 텍스트파일을 열어 확인하여도 문자가 표현이 벗어남

---
+++ 22.09.24study4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._24study4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream("test.txt", FileMode.Create))) 
+            {
+                bw.Write(12);
+                bw.Write(3.14f);
+                bw.Write("Hello World");
+            }
+        }
+    }
+}
