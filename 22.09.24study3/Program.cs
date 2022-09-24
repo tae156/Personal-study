b012d3c4b83e960ffd104c5548f38aa6696a5c65
tae156 <103081544+tae156@users.noreply.github.com>
2022-09-25 02:39:51 +0900

    6.3 실습 프로젝트 생성
    (이 작업은 이전에 사용했던 6.2실습에서 가지고온 내용)저장되어있던 파일을 불러와서 읽어내는 방식

---
+++ 22.09.24study3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22._09._24study3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("파일명을 입력해 주세요! ");
+            string Filename = Console.ReadLine();
+
+            StreamReader sr = new StreamReader(Filename);
+            str = sr.ReadLine();
+            string[] strData1 = str.Split(new char[] { ':' });
+            int nCount = int.Parse(strData1[1]);
+            Console.WriteLine("=======================================");
+            for (int i = 0; i < nCount; i++)
+            {
+                str = sr.ReadLine();
+                string[] strData2 = str.Split(new char[] { ' ' });
+                Console.WriteLine("{0} {1} {2} {3} {4}", strData2[0], strData2[1], strData2[2], strData2[3], strData2[4]);
+            }
+            Console.WriteLine("=======================================");
+            sr.Close();
+        }
+    }
+}
